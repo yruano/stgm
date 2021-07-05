b896fef8486e257f1c1b1a27b57286c4bdd338bd
yruano <ckrmidjskk@naver.com>
2021-07-05 23:53:46 +0900

    zzz

--- src/enemy.js
+++ src/enemy.js
@@ -7,11 +7,5 @@ class Enemy {
         50, 50,
         0xffffff
         );
-    }
-    move(delta) {
-        const pos = new Phaser.Math.Vector2(this.gameObject.x, this.gameObject.y);
-        const moveVector = this.dir.normalize().scale(this.speed * delta / 1000);
-        const nextPos = pos.add(moveVector);
-        this.gameObject.setPosition(nextPos.x, nextPos.y);
     }    
 }
