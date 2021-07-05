6bde810dce1cf37f2278271c1feeadc283c083ce
yruano <ckrmidjskk@naver.com>
2021-07-05 23:45:18 +0900

    zz

--- src/enemy.js
+++ src/enemy.js
@@ -1,3 +1,17 @@
 class Enemy {
-    
+    dir = new Phaser.Math.Vector2(0, 0);
+    constructor(scene) {
+        this.gameObject = scene.add.rectangle(
+        Game.center.x,
+        Game.center.y,
+        50, 50,
+        0xffffff
+        );
+    }
+    move(delta) {
+        const pos = new Phaser.Math.Vector2(this.gameObject.x, this.gameObject.y);
+        const moveVector = this.dir.normalize().scale(this.speed * delta / 1000);
+        const nextPos = pos.add(moveVector);
+        this.gameObject.setPosition(nextPos.x, nextPos.y);
+    }    
 }
